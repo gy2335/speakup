26abea5fcc8b5268b637bf8317651e3b8fbf1900
gy2335 <graceyang465@gmail.com>
2025-10-29 01:54:38 -0400

    fixed favicon again...

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,9 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <div>
-          <img src="/speakuplogo.png" alt="Logo" className="h-24" />
+          <Link to="/home">
+            <img src="/speakuplogo.png" alt="Logo" className="h-16" />
+          </Link>
         </div>
 
         {/* Desktop View */}
