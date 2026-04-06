49714138588b21f4f6ea319c94000389e94e2b50
Mabel Yang <yangmabeltt@gmail.com>
2026-04-06 12:48:36 -0400

    revert

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 
   return (
     <nav className="w-full bg-gradient-to-r from-[#FFE8A1] via-[#FDF5E6] to-[#B0CDEB] border-b-4 border-[#173B64] sticky top-0 left-0 z-50 transition-all">
-      <div className="-mt-0.5 max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo + Title */}
         <div>
           <Link to="/home" className="flex items-center space-x-3 group">
@@ -40,7 +40,7 @@ export default function Navbar() {
             <Link
               key={page.name}
               to={page.path}
-              className="px-5 py-2.5 rounded-full font-bold text-lg text-[#173B64] border-2 border-[#173B64] shadow-[3px_3px_0px_#173B64] hover:shadow-[5px_5px_0px_#173B64] hover:-translate-y-1 transition-all duration-200"
+              className={`px-5 py-2.5 rounded-full font-bold text-lg text-[#173B64] border-2 border-[#173B64] shadow-[3px_3px_0px_#173B64] hover:shadow-[5px_5px_0px_#173B64] hover:-translate-y-1 transition-all duration-200`}
             >
               {page.name}
             </Link>
@@ -53,9 +53,9 @@ export default function Navbar() {
           onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         >
           {mobileMenuOpen ? (
-            <span className="text-3xl">&#10005;</span> {/* x button */}
+            <span className="text-3xl">&#10005;</span> // x button
           ) : (
-            <span className="text-3xl">&#9776;</span> {/* hamburger */}
+            <span className="text-3xl">&#9776;</span> // hamburger
           )}
         </button>
       </div>
