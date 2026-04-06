11ee89b88151bcd50731890b04bac3f85c88b23e
Mabel Yang <yangmabeltt@gmail.com>
2026-04-06 12:43:53 -0400

    fix2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
             <Link
               key={page.name}
               to={page.path}
-              className={`px-5 py-2.5 rounded-full font-bold text-lg text-[#173B64] border-2 border-[#173B64] shadow-[3px_3px_0px_#173B64] hover:shadow-[5px_5px_0px_#173B64] hover:-translate-y-1 transition-all duration-200`}
+              className="px-5 py-2.5 rounded-full font-bold text-lg text-[#173B64] border-2 border-[#173B64] shadow-[3px_3px_0px_#173B64] hover:shadow-[5px_5px_0px_#173B64] hover:-translate-y-1 transition-all duration-200"
             >
               {page.name}
             </Link>
